cb225054565764b9a5772806f91ff6db148eca89
Dave Smith <davesmith.00000@gmail.com>
2021-06-23 00:26:49 +0100

    Rejigged toy game: Can't load shader asset like that, need to http request

--- toy-game/src/main/scala/indigotoy/IndigoToy.scala
+++ toy-game/src/main/scala/indigotoy/IndigoToy.scala
@@ -1,41 +1,87 @@
 package indigotoy
 
 import indigo._
+import indigo.ShaderPrimitive._
+
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("IndigoGame")
-object IndigoToy extends IndigoDemo[Unit, Unit, Unit, Unit] {
+object IndigoToy extends IndigoDemo[Size, Size, Unit, Size]:
 
   val eventFilters: EventFilters =
     EventFilters.Permissive
 
-  def boot(flags: Map[String, String]): Outcome[BootResult[Unit]] =
+  def boot(flags: Map[String, String]): Outcome[BootResult[Size]] =
+    val gameViewport =
+      (flags.get("width"), flags.get("height")) match {
+        case (Some(w), Some(h)) =>
+          GameViewport(w.toInt, h.toInt)
+
+        case _ =>
+          GameViewport(320, 200)
+      }
+
+    val maybeGist = flags.get("gist")
+    val assetName = AssetName("shader gist")
+    val assets: Set[AssetType] = maybeGist match
+      case Some(p) =>
+        Set(AssetType.Text(assetName, AssetPath(s"https://gist.githubusercontent.com/$p/raw")))
+      case None =>
+        Set()
+
     Outcome(
-      BootResult.noData(
+      BootResult(
         GameConfig.default
+          .withViewport(gameViewport),
+        gameViewport.size
       )
+        .withAssets(assets)
+        .withShaders(ToyEntity.shader(assetName))
     )
 
-  def initialModel(startupData: Unit): Outcome[Unit] =
+  def setup(bootData: Size, assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Size]] =
+    Outcome(Startup.Success(bootData))
+
+  def initialModel(startupData: Size): Outcome[Unit] =
     Outcome(())
 
-  def initialViewModel(startupData: Unit, model: Unit): Outcome[Unit] =
-    Outcome(())
+  def initialViewModel(startupData: Size, model: Unit): Outcome[Size] =
+    Outcome(startupData)
 
-  def setup(bootData: Unit, assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Unit]] =
-    Outcome(Startup.Success(()))
-
-  def updateModel(context: FrameContext[Unit], model: Unit): GlobalEvent => Outcome[Unit] =
+  def updateModel(context: FrameContext[Size], model: Unit): GlobalEvent => Outcome[Unit] =
     _ => Outcome(model)
 
-  def updateViewModel(context: FrameContext[Unit], model: Unit, viewModel: Unit): GlobalEvent => Outcome[Unit] =
-    _ => Outcome(viewModel)
+  def updateViewModel(context: FrameContext[Size], model: Unit, viewModel: Size): GlobalEvent => Outcome[Size] =
+    case indigo.shared.events.ViewportResize(newSize) =>
+      Outcome(newSize.size)
+    case _ =>
+      Outcome(viewModel)
 
-  def present(context: FrameContext[Unit], model: Unit, viewModel: Unit): Outcome[SceneUpdateFragment] =
+  def present(context: FrameContext[Size], model: Unit, viewModel: Size): Outcome[SceneUpdateFragment] =
     Outcome(
       SceneUpdateFragment(
-        Shape.Box(Rectangle(10, 10, 100, 100), Fill.Color(RGBA.Green), Stroke.None)
+        ToyEntity(viewModel)
       )
     )
 
-}
+final case class ToyEntity(size: Size) extends EntityNode:
+  val position: Point   = Point.zero
+  val depth: Depth      = Depth(1)
+  val flip: Flip        = Flip.default
+  val ref: Point        = Point.zero
+  val rotation: Radians = Radians.zero
+  val scale: Vector2    = Vector2.one
+
+  def withDepth(newDepth: Depth): ToyEntity = this
+
+  def toShaderData: ShaderData =
+    ShaderData(ToyEntity.shaderId)
+
+object ToyEntity:
+  val shaderId: ShaderId =
+    ShaderId("toy shader")
+
+  def shader(fragProgram: AssetName): EntityShader =
+    EntityShader
+      .External(shaderId)
+      .withFragmentProgram(fragProgram)
