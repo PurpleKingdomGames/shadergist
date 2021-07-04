24cd2ed1c7df8b605c0d03420fb586288da235ed
Dave Smith <davesmith.00000@gmail.com>
2021-07-04 22:43:07 +0100

    Syntax highlighting

--- toy/src/main/scala/tools/pages/Home.scala
+++ toy/src/main/scala/tools/pages/Home.scala
@@ -10,20 +10,24 @@ object Home:
     div(_class("container"))(
       div(_class("row"))(
         div(id("indigo-container"), _class("col"))(),
-        div(_class("col"))(
-          pre(
-            code(id("foo"), _class("language-glsl"))(
+        div(_class("col"), styles("overflow" -> "scroll", "font-size" -> "0.75em"))(
+          pre()(
+            code(_class("language-glsl"))(
               gistCode.map(c => List(text(c))).getOrElse(Nil)
             )
+          ),
+          script(_type("text/javascript"))(
+            gistCode match
+              case None =>
+                text("")
+
+              case Some(_) =>
+                text(
+                  s"""window.Prism.highlightAll();"""
+                )
           )
         )
       ),
-      // div(_class("row"))(
-      //   div(_class("col"))(
-      //     input(_type("text"), placeholder("davesmith00000/43ee912e19f60b0dc1fde9905cbff832"), value(gistPath)),
-      //     button(_type("button"), onClick(Msg.LoadGist(gistPath)))(text("reload"))
-      //   )
-      // ),
       script(_type("text/javascript"), defer)(
         text(
           s"""window.indigo.IndigoGame.launch({"width": "550", "height": "400", "gist": "$gistPath"});"""
