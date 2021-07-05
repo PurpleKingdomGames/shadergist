8ea751330aef668bbb943b6717108d23620138b5
Dave Smith <davesmith.00000@gmail.com>
2021-07-05 21:56:29 +0100

    Added a description

--- shadergist-site/src/main/scala/shadergistsite/views/Home.scala
+++ shadergist-site/src/main/scala/shadergistsite/views/Home.scala
@@ -14,7 +14,10 @@ object Home:
       case Some(gist) =>
         div(_class("container"))(
           div(_class("row"))(
-            div(id("indigo-container"), _class("col-md-auto"))(),
+            div(_class("col-md-auto"))(
+              div(id("indigo-container"))(),
+              about
+            ),
             div(_class("col"), styles("overflow" -> "hidden", "font-size" -> "0.75em"))(
               pre(styles("margin" -> "0px"))(
                 code(_class("language-glsl"))(
@@ -29,3 +32,23 @@ object Home:
             )
           )
         )
+
+  def about: Html[Msg] =
+    div(styles("width" -> "400px"))(
+      br,
+      h1(text("ShaderGist")),
+      p(
+        text("ShaderGist allows you to render "),
+        a(href("https://indigoengine.io/"), target("_blank"), styles("color" -> "#DDBB00"))(text("Indigo")),
+        text(" flavoured GLSL shaders, saved as public GitHub gists.")
+      ),
+      p(
+        text("Sharing them is as simple as copying a link, try this one:"),
+        br,
+        br,
+        a(
+          href("?gist=davesmith00000/5a6b585dbc2184a2c215b16a91234c22"),
+          styles("color" -> "#DDBB00")
+        )(text("Green Fire Shader!"))
+      )
+    )
