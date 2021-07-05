dc5b887f05ad4ecaec7179b5a3a2ddfe179490db
Dave Smith <davesmith.00000@gmail.com>
2021-07-05 20:47:30 +0100

    Fixed loading on Chrome

--- shadergist-site/src/main/scala/shadergistsite/pages/Home.scala
+++ shadergist-site/src/main/scala/shadergistsite/pages/Home.scala
@@ -7,20 +7,25 @@ import shadergistsite.Styles
 
 object Home:
   def view(gistPath: String, gistCode: Option[String]): Html[Msg] =
-    div(_class("container"))(
-      div(_class("row"))(
-        div(id("indigo-container"), _class("col-md-auto"))(),
-        div(_class("col"), styles("overflow" -> "scroll", "font-size" -> "0.75em"))(
-          pre()(
-            code(_class("language-glsl"))(
-              gistCode.map(c => List(text(c))).getOrElse(Nil)
+    gistCode match
+      case None =>
+        div(_class("container"))()
+
+      case Some(gist) =>
+        div(_class("container"))(
+          div(_class("row"))(
+            div(id("indigo-container"), _class("col-md-auto"))(),
+            div(_class("col"), styles("overflow" -> "hidden", "font-size" -> "0.75em"))(
+              pre(styles("margin" -> "0px"))(
+                code(_class("language-glsl"))(
+                  text(gist)
+                )
+              )
+            )
+          ),
+          script(_type("text/javascript"), defer)(
+            text(
+              s"""window.indigo.IndigoGame.launch({"width": "400", "height": "400", "gist": "$gistPath"});"""
             )
           )
         )
-      ),
-      script(_type("text/javascript"), defer)(
-        text(
-          s"""window.indigo.IndigoGame.launch({"width": "400", "height": "400", "gist": "$gistPath"});"""
-        )
-      )
-    )
